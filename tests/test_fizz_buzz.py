ecc29b59cb3cb98f4016567bb88e58b086c464da
ABDERRHMAN8BENAICHA <dahmangot2020@gmail.com>
2025-11-04 22:46:56 +0100

    Refactoring

--- tests/test_fizz_buzz.py
+++ tests/test_fizz_buzz.py
@@ -1,29 +1,17 @@
 from src.fizz_buzz import fizz_buzz
+import pytest
 
-
-def test_fizz_buzz_3_returns_fizz():
-    assert fizz_buzz(3) == "Fizz"
-
-def test_fizz_buzz_5_returns_buzz():
-    assert fizz_buzz(5) == "Buzz"
-
-def test_fizz_buzz_10_returns_buzz():
-    assert fizz_buzz(10) == "Buzz"
-
-def test_fizz_buzz_6_returns_fizz():
-    assert fizz_buzz(6) == "Fizz"
-
-def test_fizz_buzz_9_returns_fizz():
-    assert fizz_buzz(9) == "Fizz"
-
-def test_fizz_buzz_0_returns_str_0():
-    assert fizz_buzz(0) == "0"
-
-def test_fizz_buzz_1_returns_str_1():
-    assert fizz_buzz(1) == "1"
-
-def test_fizz_buzz_2_returns_str_2():
-    assert fizz_buzz(2) == "2"
-
-def test_fizz_buzz_15_returns_fizzbuzz():
-    assert fizz_buzz(15) == "FizzBuzz"
+@pytest.mark.parametrize("number,expected_result",[
+    (0, "0"),
+    (1, "1"),
+    (2, "2"),
+    (3, "Fizz"),
+    (4, "4"),
+    (5, "Buzz"),
+    (6, "Fizz"),
+    (10, "Buzz"),
+    (15, "FizzBuzz"),
+    (30, "FizzBuzz"),
+])
+def test_fizz_buzz_number_returns_expected_result(number, expected_result):
+    assert fizz_buzz(number) == expected_result
